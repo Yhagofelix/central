cf0591b239a68b719a9176b765d3c4b914981d42
Yhago <yhagofelix@gmail.com>
2018-09-11 00:58:45 -0300

    Commit inicial

---
+++ Java/OlaMundoJavaFX/src/olamundojavafx/mensagem.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundojavafx;
+
+/**
+ *
+ * @author yhago
+ */
+class mensagem {
+
+    static void setText(String olá_Mundo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
